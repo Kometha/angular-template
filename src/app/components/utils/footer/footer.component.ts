7879784f50a5ad52f4523ae0c874550c1a8ff873
Kometha <kenethcubas2002@unitec.edu>
2025-04-20 17:06:10 -0600

    feat: add footer component with initial template and styles

---
+++ src/app/components/utils/footer/footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  standalone: true,
+  styleUrl: './footer.component.scss',
+  template: `<p>footer</p>`
+})
+export class FooterComponent {
+
+}
