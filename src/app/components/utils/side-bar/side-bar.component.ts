877f2d3592ce7fbfe2555f8fab05547af6c4cb7a
Kometha <kenethcubas2002@unitec.edu>
2025-04-20 17:48:30 -0600

    feat: enhance side bar component with navigation links and router integration

--- src/app/components/utils/side-bar/side-bar.component.ts
+++ src/app/components/utils/side-bar/side-bar.component.ts
@@ -2,18 +2,21 @@ import { Component } from '@angular/core';
 import { TreeModule } from 'primeng/tree';
 import { TreeNode } from 'primeng/api';
 import { NgClass } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [TreeModule, NgClass],
+  imports: [TreeModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss',
 })
 export class SideBarComponent {
+  constructor(private router: Router) {}
+
   departamentos: TreeNode[] = [
-    { label: 'Caja', data: { icon: 'pi pi-wallet' } },
-    { label: 'Gerencia', data: { icon: 'pi pi-briefcase' } },
+    { label: 'Caja', data: { icon: 'pi pi-wallet', routerLink: '/menu1' } },
+    { label: 'Gerencia', data: { icon: 'pi pi-briefcase', routerLink: '/menu2' } },
     { label: 'Información', data: { icon: 'pi pi-info-circle' } },
     { label: 'Mercadeo', data: { icon: 'pi pi-dollar' } },
     { label: 'Taller', data: { icon: 'pi pi-wrench' } },
@@ -24,4 +27,10 @@ export class SideBarComponent {
     { label: 'RRHH', data: { icon: 'pi pi-users' } },
     { label: 'Configuración', data: { icon: 'pi pi-cog' } },
   ];
+
+  navigateTo(node: TreeNode) {
+    if (node.data?.routerLink) {
+      this.router.navigate([node.data.routerLink]);
+    }
+  }
 }
