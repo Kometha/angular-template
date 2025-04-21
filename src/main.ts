0cb59fa6ccf0074a3f1a3b6a00cfdbf220c15615
Kometha <kenethcubas2002@unitec.edu>
2025-04-20 19:23:01 -0600

    feat: integrate router into application bootstrap for improved navigation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import 'primeicons/primeicons.css';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
+import { provideRouter } from '@angular/router';
+import { APP_ROUTES } from './app/app.routes';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideRouter(APP_ROUTES),
+  ],
+}).catch(err => console.error(err));
