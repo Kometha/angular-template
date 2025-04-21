3d2d98d077e770629fb4a9bad67097c2049bb268
Kometha <kenethcubas2002@unitec.edu>
2025-04-20 19:23:33 -0600

    feat: add Menu2 component with initial template and styles

---
+++ src/app/components/menu2/menu2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu2',
+  imports: [],
+  standalone: true,
+  template: `<p>menu2</p>`,
+  styleUrl: './menu2.component.scss',
+})
+export class Menu2Component {}
