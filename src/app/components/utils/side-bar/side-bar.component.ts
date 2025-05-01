4de1e688bd99fd3fbff70a97a6058af435fe9706
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 17:45:11 -0600

    feat: implement keyboard navigation for side-bar component

--- src/app/components/utils/side-bar/side-bar.component.ts
+++ src/app/components/utils/side-bar/side-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, HostListener, Output } from '@angular/core';
 import { TreeModule } from 'primeng/tree';
 import { TreeNode } from 'primeng/api';
 import { Router } from '@angular/router';
@@ -24,6 +24,24 @@ export class SideBarComponent {
     { label: 'Tabla', data: { icon: 'pi pi-table', routerLink: '/menu2' } },
     { label: 'Configuración', data: { icon: 'pi pi-cog' } },
   ];
+
+  // TODO: INTENTO DE PARSEO ESTO AÚN NO FUNCIONA... PERO FUNCIONARÁ
+  focusedIndex = 0; // ✅ empieza enfocado en el primer elemento
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeydown(event: KeyboardEvent) {
+    if (event.key === 'ArrowDown') {
+      event.preventDefault();
+      this.focusedIndex = (this.focusedIndex + 1) % this.departamentos.length;
+    } else if (event.key === 'ArrowUp') {
+      event.preventDefault();
+      this.focusedIndex = (this.focusedIndex - 1 + this.departamentos.length) % this.departamentos.length;
+    } else if (event.key === 'Enter' || event.key === ' ') {
+      event.preventDefault();
+      const node = this.departamentos[this.focusedIndex];
+      this.navigateTo(node);
+    }
+  }
 
   navigateTo(node: TreeNode) {
     if (node.data?.routerLink) {
