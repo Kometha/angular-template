f9aff34ec264533d1734b7ad2fb0eba7ea7c4ece
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 11:47:10 -0600

    feat: implement form layout with input fields for name, email, and password in FormHorizontalComponent

--- src/app/components/menu1/components/form-horizontal/form-horizontal.component.ts
+++ src/app/components/menu1/components/form-horizontal/form-horizontal.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-form-horizontal',
-  imports: [],
+  imports: [FormsModule, InputTextModule],
   standalone: true,
-  template: ` <div class="flex flex-col gap-4"></div> `,
+  template: `
+
+    <div class="mb-5 flex items-center space-x-4">
+      <div class="w-1/4">
+        <label for="name" class="text-sm text-gray-400">Nombre completo:</label>
+      </div>
+      <div class="w-3/4">
+        <input
+          id="name"
+          placeholder="Keneth Moises Cubas López"
+          type="text"
+          pInputText
+          class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+        />
+      </div>
+    </div>
+
+    <div class="mb-5 flex items-center space-x-4">
+      <div class="w-1/4">
+        <label for="name" class="text-sm text-gray-400">Correo Electrónico:</label>
+      </div>
+      <div class="w-3/4">
+        <input
+          id="name"
+          placeholder="kometha@template.com"
+          type="email"
+          pInputText
+          class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+        />
+      </div>
+    </div>
+    <div class="mb-5 flex items-center space-x-4">
+      <div class="w-1/4">
+        <label for="name" class="text-sm text-gray-400">Contraseña</label>
+      </div>
+      <div class="w-3/4">
+        <input
+          id="name"
+          placeholder="•••••••••••••••••"
+          type="password"
+          pInputText
+          class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+        />
+      </div>
+    </div>
+  `,
   styleUrl: './form-horizontal.component.scss',
 })
 export class FormHorizontalComponent {}
