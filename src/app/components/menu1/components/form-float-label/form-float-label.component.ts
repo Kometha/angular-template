6a225f79441f816536cd79c595ebe3f749135d26
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 10:26:35 -0600

    feat: create FormFloatLabelComponent with initial structure and styles

---
+++ src/app/components/menu1/components/form-float-label/form-float-label.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-float-label',
+  imports: [],
+  template: ``,
+  styleUrl: './form-float-label.component.scss'
+})
+export class FormFloatLabelComponent {
+
+}
