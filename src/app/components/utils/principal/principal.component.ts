5ad871d4bd4855da6588ae75f0afbf32ac9a19f7
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 15:36:10 -0600

    feat: update layout of PrincipalComponent for improved structure and styling

--- src/app/components/utils/principal/principal.component.ts
+++ src/app/components/utils/principal/principal.component.ts
@@ -3,15 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-principal',
   imports: [],
-  template: `<div class="flex justify-center items-center flex-col h-screen">
+  template: `<div class="container">
     <img
       src="https://i.ibb.co/FqHfqxBh/kometh-logo2.png"
       alt="Kometha Logo"
       class="mb-6 w-48 h-auto"
     />
-    <p class="text-white text-sm">
-      v0.1
-    </p>
+    <p class="text-white text-sm">v0.1</p>
     <p class="text-white text-sm">
       Copyright {{ anioActual }} Kometha Technologies ©
     </p>
