818766eeef55ed116ed7600e30db1376c18bf386
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 08:41:23 -0600

    feat: update Menu1Component to include form components and enhance layout

--- src/app/components/menu1/menu1.component.ts
+++ src/app/components/menu1/menu1.component.ts
@@ -1,15 +1,46 @@
 import { Component } from '@angular/core';
+import { FormVerticalComponent } from './components/form-vertical/form-vertical.component';
+import { FormVerticalGridComponent } from './components/form-vertical-grid/form-vertical-grid.component';
+import { FormHorizontalComponent } from './components/form-horizontal/form-horizontal.component';
 
 @Component({
   selector: 'app-menu1',
   host: { class: 'block h-full w-full' },
-  imports: [],
+  imports: [
+    FormVerticalComponent,
+    FormHorizontalComponent,
+    FormVerticalGridComponent,
+  ],
   standalone: true,
   template: `
-    <div class="h-screen grid grid-cols-6 grid-rows-5 gap-4">
-      <div class="bg-blue-500 col-span-2 row-span-2">1</div>
-      <div class="bg-blue-500 col-span-2 row-span-2 col-start-3">2</div>
-      <div class="bg-blue-500 col-span-2 row-span-2 col-start-5">3</div>
+    <div class="h-screen grid grid-cols-6 grid-rows-5 gap-3">
+      <div
+        class="text-left bg-neutral-800 col-span-3 row-span-2 col-start-4 row-start-1 p-6 rounded-xl"
+      >
+        <h1 class="text-2xl font-semibold mb-4">Form Horizontal</h1>
+        <app-form-horizontal></app-form-horizontal>
+      </div>
+      <div
+        class="text-left bg-neutral-800 col-span-3 row-span-2 col-start-4 row-start-3 p-6 rounded-xl"
+      >
+        5
+      </div>
+      <div
+        class="text-left bg-neutral-800 col-span-3 row-span-3 col-start-1 row-start-1 p-6 rounded-xl"
+      >
+        <h1 class="text-2xl font-semibold mb-4">Form Vertical</h1>
+        <app-form-vertical></app-form-vertical>
+      </div>
+
+      <div
+        class="text-left bg-neutral-800 col-span-3 row-span-2 row-start-4 p-6 rounded-xl"
+      >
+        <h1 class="text-2xl font-semibold mb-4">Form Vertical Grid</h1>
+        <app-form-vertical-grid></app-form-vertical-grid>
+      </div>
+      <div class="text-left bg-neutral-800 col-span-3 col-start-4 row-start-5 p-6 rounded-xl">
+        8
+      </div>
     </div>
   `,
   styleUrl: './menu1.component.scss',
