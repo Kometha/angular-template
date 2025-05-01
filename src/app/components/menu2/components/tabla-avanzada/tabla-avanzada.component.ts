e6068fd8279c79e1daedab34ed2a9a8a056088b2
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 14:39:37 -0600

    feat: create TablaAvanzadaComponent with basic template and styles

---
+++ src/app/components/menu2/components/tabla-avanzada/tabla-avanzada.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tabla-avanzada',
+  imports: [],
+  template: `<p>Tabla Avanzada</p>`,
+  standalone: true,
+  styleUrl: './tabla-avanzada.component.scss'
+})
+export class TablaAvanzadaComponent {
+
+}
