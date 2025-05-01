6f5211d6e73845da457cf907a149a63a9401de11
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 17:38:47 -0600

    feat: enhance header component with avatar and keyboard toggle functionality

--- src/app/components/utils/header/header.component.ts
+++ src/app/components/utils/header/header.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { DrawerModule } from 'primeng/drawer';
 import { SideBarComponent } from '../side-bar/side-bar.component';
 import { ButtonModule } from 'primeng/button';
 import { Router } from '@angular/router';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
 
 @Component({
   selector: 'app-header',
-  imports: [ButtonModule, DrawerModule, SideBarComponent],
+  imports: [
+    ButtonModule,
+    DrawerModule,
+    SideBarComponent,
+    AvatarModule,
+    AvatarGroupModule,
+  ],
   standalone: true,
   styleUrl: './header.component.scss',
   template: `
@@ -14,6 +22,12 @@ import { Router } from '@angular/router';
       <div class="flex items-center w-full">
         <p-button (click)="visible = true" icon="pi pi-bars"></p-button>
         <div class="flex-grow"></div>
+        <p-avatar
+          icon="pi pi-user"
+          class="mr-2 cursor-pointer"
+          size="large"
+          shape="circle"
+        />
         <p-button (click)="logout()" icon="pi pi-sign-out"></p-button>
       </div>
       <p-drawer [(visible)]="visible" header="Menú" [modal]="true">
@@ -26,8 +40,18 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
   visible = false;
+
   constructor(private router: Router) {}
+
   logout() {
     this.router.navigate(['/login']);
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    if (event.ctrlKey && event.key.toLowerCase() === 'b') {
+      event.preventDefault();
+      this.visible = !this.visible;
+    }
   }
 }
