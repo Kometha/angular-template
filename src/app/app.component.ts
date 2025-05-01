5c88bd2dde53434841f08ccae832b5b7aff5cca3
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 06:19:16 -0600

    refactor: replace BreadcrumComponent import with BreadcrumbService and comment out breadcrumb template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { DrawerModule } from 'primeng/drawer';
 import { HeaderComponent } from './components/utils/header/header.component';
 import { FooterComponent } from './components/utils/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { BreadcrumComponent } from './components/utils/breadcrum/breadcrum.component';
+import { BreadcrumbService } from './components/utils/breadcrum/breadcrum.component';
 
 @Component({
   selector: 'app-root',
@@ -15,13 +15,12 @@ import { BreadcrumComponent } from './components/utils/breadcrum/breadcrum.compo
     HeaderComponent,
     FooterComponent,
     RouterOutlet,
-    BreadcrumComponent,
   ],
   template: `
     <div class="app-layout flex flex-col min-h-screen">
       <app-header></app-header>
 
-      <app-breadcrumb></app-breadcrumb>
+      <!-- <app-breadcrumb></app-breadcrumb> -->
 
       <!-- Contenedor central -->
       <div class="flex-1">
