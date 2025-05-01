f72e5a5472137393ef3ebf8fbef78971605505b3
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 10:11:30 -0600

    feat: create FormHorizontalComponent with initial template and styles

---
+++ src/app/components/menu1/components/form-horizontal/form-horizontal.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-horizontal',
+  imports: [],
+  standalone: true,
+  template: ` <div class="flex flex-col gap-4"></div> `,
+  styleUrl: './form-horizontal.component.scss',
+})
+export class FormHorizontalComponent {}
