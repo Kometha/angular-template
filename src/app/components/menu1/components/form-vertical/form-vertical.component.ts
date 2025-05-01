0008daa006dd8da9d0e77465df0907203d127e6e
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 10:11:23 -0600

    feat: refactor FormVerticalComponent to use external template file

--- src/app/components/menu1/components/form-vertical/form-vertical.component.ts
+++ src/app/components/menu1/components/form-vertical/form-vertical.component.ts
@@ -7,44 +7,7 @@ import { Select } from 'primeng/select';
   selector: 'app-form-vertical',
   imports: [FormsModule, Select, InputTextModule],
   standalone: true,
-  template: `
-    <div class="mb-5">
-      <label for="name" class="block text-sm mb-2">Nombre Completo</label>
-      <input
-        id="name"
-        type="text"
-        pInputText
-        class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-      />
-    </div>
-    <div class="mb-5">
-      <label for="email" class="block text-sm mb-2">Email</label>
-      <input
-        id="email"
-        type="email"
-        pInputText
-        class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-      />
-    </div>
-    <div class="mb-5">
-      <label for="age" class="block text-sm mb-2">Edad</label>
-      <input
-        id="age"
-        type="number"
-        pInputText
-        class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
-      />
-    </div>
-    <div>
-      <label for="age" class="block text-sm mb-2">Ciudades</label>
-      <p-select
-        [options]="cities"
-        optionLabel="name"
-        placeholder="Select a City"
-        class="w-full"
-      />
-    </div>
-  `,
+  templateUrl: './form-vertical.component.html',
   styleUrl: './form-vertical.component.scss',
 })
 export class FormVerticalComponent {
