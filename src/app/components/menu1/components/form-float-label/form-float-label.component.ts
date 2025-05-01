9308ea810abc68aebc64e4144b5615b4cbbd285a
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 11:47:13 -0600

    feat: implement form layout with float labels for user input fields in FormFloatLabelComponent

--- src/app/components/menu1/components/form-float-label/form-float-label.component.ts
+++ src/app/components/menu1/components/form-float-label/form-float-label.component.ts
@@ -1,11 +1,63 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabel } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-form-float-label',
-  imports: [],
-  template: ``,
-  styleUrl: './form-float-label.component.scss'
-})
-export class FormFloatLabelComponent {
+  imports: [FormsModule, InputTextModule, FloatLabel],
+  template: `
+    <div class="mb-5">
+      <p-floatlabel variant="on">
+        <input
+          pInputText
+          id="on_label"
+          autocomplete="off"
+          class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+        />
+        <label for="on_label">Nombres</label>
+      </p-floatlabel>
+    </div>
+    <div class="mb-5">
+      <p-floatlabel variant="on">
+        <input
+          pInputText
+          id="on_label"
+          autocomplete="off"
+          class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+        />
+        <label for="on_label">Apellidos</label>
+      </p-floatlabel>
+    </div>
 
-}
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <!-- Primera fila -->
+      <div class="mb-5">
+        <p-floatlabel variant="on">
+          <input
+            pInputText
+            type="number"
+            id="on_label"
+            autocomplete="off"
+            class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+          />
+          <label for="on_label">Edad</label>
+        </p-floatlabel>
+      </div>
+      <div class="mb-5">
+        <p-floatlabel variant="on">
+          <input
+            pInputText
+            type="number"
+            id="on_label"
+            autocomplete="off"
+            class="w-full p-3 rounded-md bg-white text-gray-800 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 transition"
+          />
+          <label for="on_label">Salario</label>
+        </p-floatlabel>
+      </div>
+    </div>
+  `,
+  styleUrl: './form-float-label.component.scss',
+})
+export class FormFloatLabelComponent {}
