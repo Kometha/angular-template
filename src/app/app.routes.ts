363f701ac8a1f79188d27ed763182249698332cd
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 06:19:13 -0600

    feat: update routing to redirect to LoginComponent by default

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { Menu1Component } from './components/menu1/menu1.component';
 import { Menu2Component } from './components/menu2/menu2.component';
+import { LoginComponent } from './components/utils/login/login.component';
 
 
 export const APP_ROUTES: Routes = [
   { path: 'menu1', component: Menu1Component },
   { path: 'menu2', component: Menu2Component },
-  { path: '', redirectTo: '/menu1', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
