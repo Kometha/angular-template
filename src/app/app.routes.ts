c28fc7368ca9ae5e3153945021d1368e5df56a54
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 12:28:35 -0600

    fix: update routing to use FormulariosExamplesComponent for menu1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { TablasExamplesComponent } from './components/menu1/tablas-examples.component';
+import { FormulariosExamplesComponent } from './components/menu1/formularios-examples.component';
 import { Menu2Component } from './components/menu2/menu2.component';
 import { LoginComponent } from './components/utils/login/login.component';
 import { MainLayoutComponent } from './components/utils/main-layout/main-layout.component';
@@ -10,7 +10,7 @@ export const APP_ROUTES: Routes = [
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: 'menu1', component: TablasExamplesComponent },
+      { path: 'menu1', component: FormulariosExamplesComponent },
       { path: 'menu2', component: Menu2Component },
     ],
   },
