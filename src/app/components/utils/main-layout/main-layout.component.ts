925419d04437dabe32c30a9943d1118c5b2e5bf0
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 06:31:37 -0600

    feat: add MainLayoutComponent with header and footer structure

---
+++ src/app/components/utils/main-layout/main-layout.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
+import { HeaderComponent } from '../header/header.component';
+
+@Component({
+  selector: 'app-main-layout',
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  standalone: true,
+  styleUrl: './main-layout.component.scss',
+  template: `
+    <div class="app-layout flex flex-col min-h-screen">
+      <app-header></app-header>
+      <div class="flex-1">
+        <router-outlet></router-outlet>
+      </div>
+      <app-footer></app-footer>
+    </div>
+  `,
+})
+export class MainLayoutComponent {}
