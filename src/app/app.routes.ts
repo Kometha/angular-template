731d83fef55c7b840a4b636ec2da5d558b9ff898
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 12:25:51 -0600

    fix: update routing to use TablasExamplesComponent instead of Menu1Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { Menu1Component } from './components/menu1/menu1.component';
+import { TablasExamplesComponent } from './components/menu1/tablas-examples.component';
 import { Menu2Component } from './components/menu2/menu2.component';
 import { LoginComponent } from './components/utils/login/login.component';
 import { MainLayoutComponent } from './components/utils/main-layout/main-layout.component';
@@ -10,7 +10,7 @@ export const APP_ROUTES: Routes = [
     path: '',
     component: MainLayoutComponent,
     children: [
-      { path: 'menu1', component: Menu1Component },
+      { path: 'menu1', component: TablasExamplesComponent },
       { path: 'menu2', component: Menu2Component },
     ],
   },
