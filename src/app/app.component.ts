5aa64e85614f889780afd5fd2b1bad712e5180d5
Kometha <kenethcubas2002@unitec.edu>
2025-05-01 06:31:41 -0600

    refactor: simplify AppComponent by removing unused imports and template structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,11 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
-import { DrawerModule } from 'primeng/drawer';
-import { HeaderComponent } from './components/utils/header/header.component';
-import { FooterComponent } from './components/utils/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { BreadcrumbService } from './components/utils/breadcrum/breadcrum.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    ButtonModule,
-    DrawerModule,
-    HeaderComponent,
-    FooterComponent,
-    RouterOutlet,
-  ],
-  template: `
-    <div class="app-layout flex flex-col min-h-screen">
-      <app-header></app-header>
-
-      <!-- <app-breadcrumb></app-breadcrumb> -->
-
-      <!-- Contenedor central -->
-      <div class="flex-1">
-        <!--  ✅ -->
-        <router-outlet class="block"></router-outlet>
-        <!--  ✅ -->
-      </div>
-
-      <app-footer></app-footer>
-    </div>
-  `,
+  imports: [RouterOutlet],
+  template: `<router-outlet></router-outlet>`,
 })
-export class AppComponent {
-  visible = false;
-}
+export class AppComponent {}
