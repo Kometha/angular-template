773e627b530f50c2c35905d429bb67293b329e75
Kometha <kenethcubas2002@unitec.edu>
2025-05-03 19:32:56 -0600

    feat: implement AuthService for user authentication and profile management

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import {
+  AuthChangeEvent,
+  AuthSession,
+  createClient,
+  Session,
+  SupabaseClient,
+  User,
+} from '@supabase/supabase-js';
+import { environment } from '../../environments/environment';
+import { Profile } from '../models/profile';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private supabase!: SupabaseClient;
+  _session: AuthSession | null = null;
+
+  constructor() {
+    this.supabase = createClient(
+      environment.supabaseUrl,
+      environment.supabaseKey
+    );
+  }
+
+  get session() {
+    this.supabase.auth.getSession().then(({ data, error }) => {
+      this._session = data.session;
+    });
+    return this._session;
+  }
+
+  async profile(user: User) {
+    const { data, error } = await this.supabase
+      .from('profiles')
+      .select('*')
+      .eq('id', user.id)
+      .single();
+    if (error) {
+      console.error('Error fetching profile:', error);
+      return null;
+    }
+    return data;
+  }
+
+  authChanges(
+    callback: (event: AuthChangeEvent, session: Session | null) => void
+  ) {
+    return this.supabase.auth.onAuthStateChange(callback);
+  }
+
+  signIn(email: string) {
+    return this.supabase.auth.signInWithOtp({ email });
+  }
+
+  signOut() {
+    return this.supabase.auth.signOut();
+  }
+
+  updateProfile(profile: Profile) {
+    const update = {
+      ...profile,
+      updated_at: new Date(),
+    };
+    return this.supabase.from('profiles').upsert(update);
+  }
+}
