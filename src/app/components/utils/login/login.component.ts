d449e92edb12f33f320f5f2368af25f0ee75ddad
Kometha <kenethcubas2002@unitec.edu>
2025-05-03 21:42:01 -0600

    feat: remove unused ingresar method from login component

--- src/app/components/utils/login/login.component.ts
+++ src/app/components/utils/login/login.component.ts
@@ -165,11 +165,6 @@ export class LoginComponent {
     });
   }
 
-  ingresar() {
-    // Redireccionar a la ruta /menu1
-    this.router.navigate(['inicio']);
-  }
-
   async onSubmit(): Promise<void> {
     try {
       this.loading = true;
