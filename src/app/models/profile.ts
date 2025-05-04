3b720b3cac9d7dd1cfc64e381cb01b086d91a872
Kometha <kenethcubas2002@unitec.edu>
2025-05-03 19:33:01 -0600

    feat: add Profile interface for user profile management

---
+++ src/app/models/profile.ts
@@ -0,0 +1,18 @@
+export interface Profile {
+  id: string;
+  username: string;
+  website: string;
+  avatar_url: string;
+
+}
+
+
+// updated_at: string;
+// created_at: string;
+// full_name: string;
+// bio: string;
+// location: string;
+// twitter_username: string;
+// github_username: string;
+// website_url: string;
+// company_name: string;
