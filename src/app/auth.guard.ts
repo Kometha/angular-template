9d6a61092e4e71157aac851727c557a78b0502c7
Kometha <kenethcubas2002@unitec.edu>
2025-05-03 22:40:50 -0600

    feat: refactor AuthGuard to use session observable for route activation

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,17 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
+import { map, filter } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
 export class AuthGuard implements CanActivate {
   constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate(): boolean {
-    if (this.auth.session) {
-      return true;
-    } else {
-      this.router.navigate(['/account']);
-      return false;
-    }
+  canActivate() {
+    return this.auth.session$.pipe(
+      filter((session) => session !== undefined), // Espera a que cargue la sesión
+      map((session) => {
+        if (session) {
+          return true;
+        } else {
+          this.router.navigate(['/login']);
+          return false;
+        }
+      })
+    );
   }
 }
