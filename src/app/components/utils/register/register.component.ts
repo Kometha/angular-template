e9be4ef6152f4597fa8dee67cda2559a49053274
Kometha <kenethcubas2002@unitec.edu>
2025-05-06 19:44:07 -0600

    feat: crear componente de registro para la aplicación

---
+++ src/app/components/utils/register/register.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  template: `
+    <div>
+      <h1>Register</h1>
+    </div>
+  `,
+  styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+
+}
